bcb6730a624f78fc642a6604109e1b99c9632fc0
VisnjaM <viniamontt@gmail.com>
2023-05-18 14:15:25 -0400

    incorporo_link_descriptivo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'galeria/galeria'
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "galeria",                to:"pages#galeria",           as:"galeria"
+  get "galeria",                to:"galeria#galeria",           as:"galeria"
   # Defines the root path route ("/")
    root "home#index"
 end
